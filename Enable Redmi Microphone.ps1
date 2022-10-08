31dad28018fd5a16d1b06eb2795824f875846220
Andrei Sizyi <andreisizyi@mavericks.agency>
2022-10-08 16:55:30 +0300

    init

---
+++ Enable Redmi Microphone.ps1
@@ -0,0 +1,7 @@
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
+}
+Get-PnpDevice -FriendlyName "*Redmi AirDots 3 Hands-Free AG*" | Enable-PnpDevice -A
